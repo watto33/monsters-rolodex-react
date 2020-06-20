197c30582aebee64d0c3cdada09bb1763d70f25d
Sriraam N <sriraamwatto33@gmail.com>
2020-06-20 11:32:08 +0530

    Updates

---
+++ precache-manifest.84e3c65943b612a112f1b397adc62762.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e80bad7f763dd5b31780e7f759e3a8e",
+    "url": "/monsters-rolodex-react/index.html"
+  },
+  {
+    "revision": "86371d3fed9e4a5e5a7a",
+    "url": "/monsters-rolodex-react/static/css/main.d8abb05a.chunk.css"
+  },
+  {
+    "revision": "12240f73676d8f2fbd85",
+    "url": "/monsters-rolodex-react/static/js/2.a3c82928.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex-react/static/js/2.a3c82928.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86371d3fed9e4a5e5a7a",
+    "url": "/monsters-rolodex-react/static/js/main.e4a0b057.chunk.js"
+  },
+  {
+    "revision": "4984d3118c6c771f0983",
+    "url": "/monsters-rolodex-react/static/js/runtime-main.42c5dd98.js"
+  }
+]);
